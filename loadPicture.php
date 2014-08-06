25c1ddc6dc11ef2b4c1f7c15909a50b99f57ecd3
katjajohanna <johanna@Johannas-MacBook.local>
2014-08-06 07:27:45 +0300

    Removed deleted files for picture handling

--- loadPicture.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("dbopen.php");
-$result = $mysqli->query("SELECT * FROM picture WHERE id = " . addslashes($_GET["id"]) . ";");
-while ($row = $result->fetch_assoc()) {
-	$bin = base64_decode($row["picture"]);
-	header("Content-Type: image/png");
-    header("Content-Length: " . strlen($bin));
-    echo $bin;
-}
